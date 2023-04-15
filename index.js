4a560da81d71a8fc9e30252dd364ab8a454e8136
Dheeraj <vinod.asset.tnm@gmail.com>
2023-04-15 10:48:09 +0530

    Added new routes /login

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.get('/signup' , (req,res) => {
             <title>Authorisation page</title>
         </head>
         <body>
+        <h1 align = center>Signup Page</h1>
           <h1>Kindly enter your authorization details</h1>
           <form id = "#signup-form">
             <label for="email">Email:</label>
@@ -118,18 +119,73 @@ app.post('/signup', function(req, res) {
 
   // Return a 200 (OK) status code to the client
   res.status(200).send('User created successfully');
+  window.location.href("/login");
 })
 
+app.get('/login' , (req,res) => {
+
+  const html = `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset = "UTF-8">
+          <title>Authorisation page</title>
+      </head>
+      <body>
+      <h1 align = center>Login Page</h1>
+        <h1>Kindly enter your authorization details</h1>
+        <form id = "#signup-form">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email">
+
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password">
+
+          <input type="submit" value="Submit">
+         </form>
+
+         <script>
+          const form = document.querySelector('#signup-form');
+
+          form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(form);
+            const response = await fetch('/login', {
+              method: 'POST',
+              body: formData
+            });
+
+            if (response.ok) {
+              console.log('User signed up successfully');
+            } else {
+              console.error('Failed to sign up user');
+            }
+          });
+        </script>
+
+      </body>
+    </html>
+  `
+  res.send(html);
+})
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  const {email , password} = req.body;
+  console.log("Sending data to login");
+
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
 
+  const exists = USERS.some(user=> user.email === email && user.password === password)
 
   // If the password is the same, return back 200 status code to the client
+
+  if(exists === true)
+    res.status(200);
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
 
